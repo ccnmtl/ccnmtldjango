ed9e2a444a5df477ba40ceea313738c5e768f04a
anders <anders@1f418930-7ff8-0310-b8ad-c653122473bc>
2008-11-14 21:41:30 +0000

    i think it should finally work properly now, including all directories
    git-svn-id: http://svn.ccnmtl.columbia.edu/ccnmtldjango/trunk@12849 1f418930-7ff8-0310-b8ad-c653122473bc

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(name='ccnmtldjango',
       install_requires = ["PasteScript"],
       zip_safe=False,
       package_data = {'' : ['*.*']},
+      include_package_data=True,
       entry_points="""
       [paste.paster_create_template]
       ccnmtldjango = ccnmtldjango:CcnmtlDjangoTemplate
