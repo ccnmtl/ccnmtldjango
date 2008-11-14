c516e1f2029ecaf57840db530aa6c068c956baad
anders <anders@1f418930-7ff8-0310-b8ad-c653122473bc>
2008-11-14 21:21:47 +0000

    actually delete the ve directory
    git-svn-id: http://svn.ccnmtl.columbia.edu/ccnmtldjango/trunk@12848 1f418930-7ff8-0310-b8ad-c653122473bc

--- ccnmtldjango/template/bootstrap.py
+++ ccnmtldjango/template/bootstrap.py
@@ -2,14 +2,13 @@
 import os
 import sys
 import subprocess
+import shutil
 
 pwd = os.path.dirname(__file__)
 vedir = os.path.join(pwd,"ve")
 
-try:
-    os.removedirs(vedir)
-except:
-    pass
+if os.path.exists(vedir):
+    shutil.rmtree(vedir)
 
 subprocess.call(["python",os.path.join(pwd,"create-ve.py")])
 subprocess.call(["python",os.path.join(pwd,"ve-bootstrap.py"),vedir])
