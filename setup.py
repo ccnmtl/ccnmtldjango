c2c84056f7934de81c970a0b3760d583d7cce4ed
anders <anders@1f418930-7ff8-0310-b8ad-c653122473bc>
2008-11-14 21:41:54 +0000

    call it a version bump
    git-svn-id: http://svn.ccnmtl.columbia.edu/ccnmtldjango/trunk@12850 1f418930-7ff8-0310-b8ad-c653122473bc

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='ccnmtldjango',
-      version="0.1",
+      version="0.2",
       author="anders pearson",
       description="Django template for CCNMTL",
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
