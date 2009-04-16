dd589c74f1c493707d30c1716038441e81501ad9
Anders Pearson <anders@columbia.edu>
2009-04-16 14:02:28 -0400

    include tweaked pip and hopefully support virtualenv 1.3.3

--- ccnmtldjango/template/bootstrap.py
+++ ccnmtldjango/template/bootstrap.py
@@ -10,6 +10,8 @@ vedir = os.path.join(pwd,"ve")
 if os.path.exists(vedir):
     shutil.rmtree(vedir)
 
-subprocess.call(["pip","install","-E",os.path.join(pwd,"ve"),
+subprocess.call(["python",os.path.join(pwd,"pip.py"),"install",
+                 "-E",os.path.join(pwd,"ve"),
+                 "--enable-site-packages",
                  "--requirement",os.path.join(pwd,"requirements/apps.txt")])
 
