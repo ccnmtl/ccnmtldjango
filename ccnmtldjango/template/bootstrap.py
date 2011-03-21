3a4420be2516cc890042fd1009372e2a7b0c253b
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2011-03-21 12:53:53 -0400

    don't let pip go to the network to find packages -- better to fail early if we're missing something in our source distributions

--- ccnmtldjango/template/bootstrap.py
+++ ccnmtldjango/template/bootstrap.py
@@ -13,6 +13,7 @@ if os.path.exists(vedir):
 subprocess.call(["python",os.path.join(pwd,"pip.py"),"install",
                  "-E",os.path.join(pwd,"ve"),
                  "--enable-site-packages",
+                 "--index-url=''",
                  "--requirement",os.path.join(pwd,"requirements/apps.txt")])
 subprocess.call([os.path.join(vedir,"bin/easy_install"),
                  os.path.join(pwd,"requirements/eggs/egenix_mx_base-3.1.3-py2.6-linux-x86_64.egg")])
