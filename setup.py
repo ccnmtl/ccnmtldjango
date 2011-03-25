acb934e533129bcba0b4c93e0834a46abcebc636
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2011-03-25 15:12:08 -0400

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='ccnmtldjango',
-      version="0.11.4",
+      version="0.11.5",
       author="anders pearson",
       description="Django template for CCNMTL",
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
