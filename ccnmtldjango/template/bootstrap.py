54905c9ad3c176f84c3cd3e1737f6ca3bf14d5f8
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2011-03-25 15:22:26 -0400

    Update to pip 0.8.3; pin pip and setuptools versions; no network downloads at all  * pip is, annoyingly, no longer useable in a single-file form.  * so we need to switch back to starting from a single-file virtualenv,    with a post-creation script that runs pip.  * virtualenv installs pip.  * drop in a pip sdist so that virtualenv will install that,    instead of installing a random version from the network.  * drop in a setuptools egg, and pass in --setuptools-egg-path param    to virtualenv, so that virtualenv will install that instead of    a random version from the network.
    That last feature requires a forked version of virtualenv, https://github.com/ejucovy/virtualenv
    which is the version of virtualenv.py that I included here.  See https://github.com/pypa/virtualenv/issues/89

--- ccnmtldjango/template/bootstrap.py
+++ ccnmtldjango/template/bootstrap.py
@@ -3,17 +3,34 @@ import os
 import sys
 import subprocess
 import shutil
+import tempfile
 
-pwd = os.path.dirname(__file__)
-vedir = os.path.join(pwd,"ve")
+pwd = os.path.abspath(os.path.dirname(__file__))
+vedir = os.path.abspath(os.path.join(pwd,"ve"))
 
 if os.path.exists(vedir):
     shutil.rmtree(vedir)
 
-subprocess.call(["python",os.path.join(pwd,"pip.py"),"install",
-                 "-E",os.path.join(pwd,"ve"),
-                 "--enable-site-packages",
-                 "--index-url=''",
-                 "--requirement",os.path.join(pwd,"requirements/apps.txt")])
-subprocess.call([os.path.join(vedir,"bin/easy_install"),
-                 os.path.join(pwd,"requirements/eggs/egenix_mx_base-3.1.3-py2.6-linux-x86_64.egg")])
+import virtualenv, textwrap
+output = virtualenv.create_bootstrap_script(textwrap.dedent("""
+import os, subprocess, glob
+import os.path
+
+def after_install(options,home_dir):
+    ret = subprocess.call([os.path.join(home_dir, 'bin', 'pip'), "install",
+                          "-E", home_dir,
+                          "--enable-site-packages",
+                          "--index-url=''",
+                          "-r", os.path.join("%(pwd)s", "requirements/apps.txt")])
+    if ret: exit(ret)
+    ret = subprocess.call([os.path.join(home_dir, 'bin', 'easy_install'),
+                           os.path.join("%(pwd)s", "requirements/eggs/egenix_mx_base-3.1.3-py2.6-linux-x86_64.egg")])
+    exit(ret)
+""" % locals()))
+
+fd, bootscript = tempfile.mkstemp(".py")
+os.write(fd, output)
+os.close(fd)
+
+subprocess.call(["python2.6", bootscript, "-vvv", "--setuptools-egg-path", os.path.join(pwd, "setuptools-0.6c11-py2.6.egg"), vedir])
+
