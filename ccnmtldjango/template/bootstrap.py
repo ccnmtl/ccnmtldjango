309e1687354e5b73199c70869dab6d1d9d15456d
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2011-04-05 11:31:36 -0400

    exit early with non-zero code if first subprocess.call doesn't finish cleanly

--- ccnmtldjango/template/bootstrap.py
+++ ccnmtldjango/template/bootstrap.py
@@ -10,12 +10,15 @@ vedir = os.path.join(pwd,"ve")
 if os.path.exists(vedir):
     shutil.rmtree(vedir)
 
-subprocess.call(["python",os.path.join(pwd,"pip.py"),"install",
-                 "-E",os.path.join(pwd,"ve"),
-                 "--enable-site-packages",
-                 "--index-url=''",
-                 "--requirement",os.path.join(pwd,"requirements/apps.txt")])
-subprocess.call([os.path.join(vedir,"bin/easy_install"),
-                 '-f',os.path.join(pwd,"requirements/eggs/"),
-                 'egenix-mx-base',
-                 ])
+ret = subprocess.call(["python",os.path.join(pwd,"pip.py"),"install",
+                       "-E",os.path.join(pwd,"ve"),
+                       "--enable-site-packages",
+                       "--index-url=''",
+                       "--requirement",os.path.join(pwd,"requirements/apps.txt")])
+if ret: exit(ret)
+
+ret = subprocess.call([os.path.join(vedir,"bin/easy_install"),
+                       '-f',os.path.join(pwd,"requirements/eggs/"),
+                       'egenix-mx-base',
+                       ])
+exit(ret)
