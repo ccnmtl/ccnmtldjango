df43f6d862d5c04385c3313e5f9b0c0200e814ee
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2011-04-13 15:42:07 -0400

    * Move setuptools + pip dists to ./requirements/virtualenv_support/ for tidyness   * Use new virtualenv.py --extra-search-dir option to point to them  * Use new virtualenv.py --never-download option to be paranoid

--- ccnmtldjango/template/bootstrap.py
+++ ccnmtldjango/template/bootstrap.py
@@ -10,7 +10,12 @@ vedir = os.path.abspath(os.path.join(pwd,"ve"))
 if os.path.exists(vedir):
     shutil.rmtree(vedir)
 
-ret = subprocess.call(["python", "virtualenv.py", vedir])
+virtualenv_support_dir = os.path.abspath(os.path.join(pwd, "requirements", "virtualenv_support"))
+
+ret = subprocess.call(["python", "virtualenv.py", 
+                       "--extra-search-dir=%s" % virtualenv_support_dir,
+                       "--never-download",
+                       vedir])
 if ret: exit(ret)
 
 ret = subprocess.call([os.path.join(vedir, 'bin', 'pip'), "install",
