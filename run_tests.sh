063e9caba314ca395052182599c5390ccc0826a8
Anders Pearson <anders@columbia.edu>
2013-03-01 15:13:53 -0500

    add simple test harness

---
+++ run_tests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# install ccnmtldjango into an outer virtualenv
+rm -rf temp/
+mkdir temp
+virtualenv temp/outer-ve
+./temp/outer-ve/bin/python setup.py develop
+
+# then use that to make a test application
+cd temp
+./outer-ve/bin/paster create --template=ccnmtldjango testproject
+cd testproject
+chmod +x bootstrap.py manage.py
+
+# build it
+./bootstrap.py
+
+# run our tests (finally!)
+./manage.py test
