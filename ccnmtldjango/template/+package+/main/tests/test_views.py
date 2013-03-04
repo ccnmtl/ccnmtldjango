68b90451cda2024ce16ca0209fb41583af36793a
Anders Pearson <anders@columbia.edu>
2013-03-04 10:50:16 -0500

    add some default dummy tests since jenkins doesn't like it when zero tests are run

---
+++ ccnmtldjango/template/+package+/main/tests/test_views.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from django.test.client import Client
+
+
+class BasicTest(TestCase):
+    def setUp(self):
+        self.c = Client()
+
+    def test_root(self):
+        response = self.c.get("/")
+        self.assertEquals(response.status_code, 200)
