c988bd78f9882aa9956cbf9c4ae59c9437df3787
Anders Pearson <anders@columbia.edu>
2013-08-07 16:11:50 -0400

    recursive-includes in MANIFEST seem broken.
    work around it by auto-generating a MANIFEST with all files
    included explicitly.

---
+++ build_packages.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+import os
+import os.path
+import subprocess
+import shutil
+import sys
+
+pwd = os.path.abspath(os.path.dirname(__file__))
+vedir = os.path.abspath(os.path.join(pwd, "ve"))
+
+def make_manifest():
+    boilerplate = """recursive-include ccnmtldjango/template/ *
+include *.md
+"""
+
+    with open("MANIFEST.in", "w") as manifest:
+        manifest.write(boilerplate)
+        for (dirpath, dirnames, filenames) in os.walk("ccnmtldjango/template/"):
+            for f in filenames:
+                manifest.write("include " + os.path.join(dirpath, f) + "\n")
+
+def clear_dist_and_build():
+    if os.path.exists("dist"):
+        shutil.rmtree("dist")
+    if os.path.exists("build"):
+        shutil.rmtree("build")
+
+def setup_ve():
+    """ make a ve with the freshest versions possible of pip/setuptools/virtualenv/wheel"""
+    if os.path.exists(vedir):
+        shutil.rmtree(vedir)
+    subprocess.call(["virtualenv", vedir])
+    subprocess.call([os.path.join(vedir, "bin", "pip"), "install", "pip", "--upgrade"])
+    subprocess.call([os.path.join(vedir, "bin", "pip"), "install", "setuptools", "--upgrade"])
+    subprocess.call([os.path.join(vedir, "bin", "pip"), "install", "virtualenv", "--upgrade"])
+    subprocess.call([os.path.join(vedir, "bin", "pip"), "install", "wheel", "--upgrade"])
+
+if __name__ == "__main__":
+    make_manifest()
+    setup_ve()
+    if len(sys.argv) > 1:
+        # upload packages
+        subprocess.call([os.path.join(vedir, "bin", "python"), "setup.py", "sdist", "upload"])
+        subprocess.call([os.path.join(vedir, "bin", "python"), "setup.py", "bdist_egg", "upload"])
+        subprocess.call([os.path.join(vedir, "bin", "python"), "setup.py", "bdist_wheel", "upload"])
+    else:
+        # just build them
+        subprocess.call([os.path.join(vedir, "bin", "python"), "setup.py", "sdist"])
+        subprocess.call([os.path.join(vedir, "bin", "python"), "setup.py", "bdist_egg"])
+        subprocess.call([os.path.join(vedir, "bin", "python"), "setup.py", "bdist_wheel"])
