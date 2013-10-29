df57406e426eb2d2370296f546164095e7e17027
Anders Pearson <anders@columbia.edu>
2013-10-29 12:01:20 -0400

    pagetree -> 0.12.6

--- run_tests.sh
+++ run_tests.sh
@@ -22,3 +22,4 @@ chmod +x bootstrap.py manage.py build_lettuce_db.sh
 ./build_lettuce_db.sh
 LETTUCE_SKIP_SELENIUM=1 ./manage.py harvest --settings=testproject.settings_lettuce --verbosity=3; rm -f /tmp/lettuce-django.pid
 ./ve/bin/flake8 testproject
+killall phantomjs
