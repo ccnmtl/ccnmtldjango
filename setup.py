ed239f93598a208cdac6bacf20d07e1ce5bb0afd
Anders Pearson <anders@columbia.edu>
2013-10-30 16:05:23 -0400

    release 1.4.2. clean out some deprecation warnings

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='ccnmtldjango',
-      version="1.4.1",
+      version="1.4.2",
       author="Anders Pearson",
       author_email='anders@columbia.edu',
       description='Paste template for Django development at CCNMTL',
