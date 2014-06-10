520ea47ba92d4b767fab8d6fc0bab9c7fd271378
Anders Pearson <anders@columbia.edu>
2014-06-10 08:32:07 -0400

    flake8ion

--- build_packages.py
+++ build_packages.py
@@ -17,25 +17,36 @@ def clear_dist_and_build():
 
 
 def setup_ve():
-    """ make a ve with the freshest versions possible of pip/setuptools/virtualenv/wheel"""
+    """ make a ve with the freshest versions possible of
+    pip/setuptools/virtualenv/wheel"""
     if os.path.exists(vedir):
         shutil.rmtree(vedir)
     subprocess.call(["virtualenv", vedir])
-    subprocess.call([os.path.join(vedir, "bin", "pip"), "install", "setuptools", "--upgrade"])
-    subprocess.call([os.path.join(vedir, "bin", "pip"), "install", "pip", "--upgrade"])
-    subprocess.call([os.path.join(vedir, "bin", "pip"), "install", "virtualenv", "--upgrade"])
-    subprocess.call([os.path.join(vedir, "bin", "pip"), "install", "wheel", "--upgrade"])
+    subprocess.call([os.path.join(vedir, "bin", "pip"), "install",
+                     "setuptools", "--upgrade"])
+    subprocess.call([os.path.join(vedir, "bin", "pip"), "install",
+                     "pip", "--upgrade"])
+    subprocess.call([os.path.join(vedir, "bin", "pip"), "install",
+                     "virtualenv", "--upgrade"])
+    subprocess.call([os.path.join(vedir, "bin", "pip"), "install",
+                     "wheel", "--upgrade"])
 
 
 if __name__ == "__main__":
     setup_ve()
     if len(sys.argv) > 1:
         # upload packages
-        subprocess.call([os.path.join(vedir, "bin", "python"), "setup.py", "sdist", "upload"])
-        subprocess.call([os.path.join(vedir, "bin", "python"), "setup.py", "bdist_egg", "upload"])
-        subprocess.call([os.path.join(vedir, "bin", "python"), "setup.py", "bdist_wheel", "upload"])
+        subprocess.call([os.path.join(vedir, "bin", "python"),
+                         "setup.py", "sdist", "upload"])
+        subprocess.call([os.path.join(vedir, "bin", "python"),
+                         "setup.py", "bdist_egg", "upload"])
+        subprocess.call([os.path.join(vedir, "bin", "python"),
+                         "setup.py", "bdist_wheel", "upload"])
     else:
         # just build them
-        subprocess.call([os.path.join(vedir, "bin", "python"), "setup.py", "sdist"])
-        subprocess.call([os.path.join(vedir, "bin", "python"), "setup.py", "bdist_egg"])
-        subprocess.call([os.path.join(vedir, "bin", "python"), "setup.py", "bdist_wheel"])
+        subprocess.call([os.path.join(vedir, "bin", "python"),
+                         "setup.py", "sdist"])
+        subprocess.call([os.path.join(vedir, "bin", "python"),
+                         "setup.py", "bdist_egg"])
+        subprocess.call([os.path.join(vedir, "bin", "python"),
+                         "setup.py", "bdist_wheel"])
