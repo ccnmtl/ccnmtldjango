465003560108723e6b63cea57f669f35875bd906
Anders Pearson <anders@columbia.edu>
2014-06-10 08:26:21 -0400

    release 1.7.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='ccnmtldjango',
-      version="1.7.0",
+      version="1.7.1",
       author="Anders Pearson",
       author_email='anders@columbia.edu',
       description='Paste template for Django development at CCNMTL',
