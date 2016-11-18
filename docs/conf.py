a150d40735b1123685248d6e80a0916732515a09
Nik Nyby <nikolas@gnu.org>
2016-11-18 14:29:31 -0500

    misc. documentation updates

--- docs/conf.py
+++ docs/conf.py
@@ -47,8 +47,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'ccnmtldjango'
-copyright = u'2015, Columbia CNMTL'
-author = u'Columbia CNMTL'
+copyright = u'2015-2016, Columbia University CTL'
+author = u'Columbia University CTL'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -227,7 +227,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   (master_doc, 'ccnmtldjango.tex', u'ccnmtldjango Documentation',
-   u'Columbia CNMTL', 'manual'),
+   u'Columbia University CTL', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
