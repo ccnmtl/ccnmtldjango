f970616a55fddd3415f1594964671cc9ee1afa62
Nik Nyby <nnyby@columbia.edu>
2017-09-21 14:47:24 -0400

    Use new CTL logo

--- ccnmtldjango/__init__.py
+++ ccnmtldjango/__init__.py
@@ -5,5 +5,5 @@ import pkg_resources
 class CcnmtlDjangoTemplate(templates.Template):
     egg_plugins = ["CcnmtlDjangoTemplate"]
     _template_dir = pkg_resources.resource_filename("ccnmtldjango", "template")
-    summary = "CCNMTL Django template"
+    summary = "Columbia CTL Django template"
     required_templates = []
